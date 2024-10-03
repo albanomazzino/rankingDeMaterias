fbada6f4cf621d171333628a4ae78446c6007d49
albanomazzino <albimazzino@gmail.com>
2024-10-03 12:06:22 -0300

    Frontend service containerization
    - Created the docker image for the ranking frontend service;
    - Updated docker-compose to handle this service as well;

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const SCHOOLS_API_ENDPOINT_URL = 'http://localhost:8222/api/v1/schools'; 
-export const RANKINGS_API_ENDPOINT_URL = 'http://localhost:8222/api/v1/rankings'; 
+export const SCHOOLS_API_ENDPOINT_URL = 'http://gateway:8222/api/v1/schools'; 
+export const RANKINGS_API_ENDPOINT_URL = 'http://gateway:8222/api/v1/rankings'; 
